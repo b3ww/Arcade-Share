1397d328106a0a15496f3d14ea90619201a02422
b3W <basile.fouquet@epitech.eu>
2024-03-19 01:53:36 +0100

    [MOV] hpp in file include[ADD] script to update discord
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

--- update.sh
+++ update.sh
@@ -7,17 +7,21 @@
 
 #!/bin/bash
 
-
 repo_path=$1
+host=$2
+
 cd "$repo_path" || exit
 
 commit_message=$(git log -1 --pretty=%B)
+
 author_name=$(git log -1 --pretty=%an)
 
 temp_file=$(mktemp)
 
-echo "{\"username\": \"$author_name\", \"message\": \"$commit_message\"}" > "$temp_file"
+data="{\"username\": \"$author_name\", \"message\": \"$commit_message\"}" > "$temp_file"
 
-curl -X POST -d @"$temp_file" -H "Content-Type: application/json" http://$2/update
+curl -X POST http://$host/update \
+    --header "Content-Type: application/json" \
+    --data "$data"
 
 rm "$temp_file"
