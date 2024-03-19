6bcf8718a0b40ff0806c5fb9388975378997e07e
b3W <basile.fouquet@epitech.eu>
2024-03-19 01:58:11 +0100

    [MOV] hpp in file include[ADD] script to update discordinstall jq
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

--- update.sh
+++ update.sh
@@ -12,16 +12,16 @@ host=$2
 
 cd "$repo_path" || exit
 
-commit_message=$(git log -1 --pretty=%B)
+commit_message=$(git log -1 --pretty=%B | jq -Rs .)
 
 author_name=$(git log -1 --pretty=%an)
 
-temp_file=$(mktemp)
 
-data="{\"username\": \"$author_name\", \"message\": \"$commit_message\"}" > "$temp_file"
+data="{\"username\": \"$author_name\", \"message\": $commit_message}" > "$temp_file"
+
+echo $data
 
 curl -X POST http://$host/update \
     --header "Content-Type: application/json" \
     --data "$data"
 
-rm "$temp_file"
